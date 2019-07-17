546966cc2b56bf241ec3cb3ef87d066d7d09f4ba
Razvan Popa <razvan.popa@softvision.ro>
2019-07-17 18:30:20 +0300

    3 - reading input data

--- app.js
+++ app.js
@@ -6,18 +6,42 @@ var budgetController = (function(){
 
 // UI CONTROLLER
 var UIController = (function(){
+
+    var DOMstrings = {
+        inputType: '.add__type',
+        inputDescription: '.add__description',
+        inputValue: '.add__value',
+        inputButton: '.add__btn',
+
+    }
     
-    // Some controller
+    return {
+        getinput: function(){
+            // Get values from inputs
+            return{
+                type: document.querySelector(DOMstrings.inputType).value, // Will be either inc or exp.
+                description: document.querySelector(DOMstrings.inputDescription).value,
+                value: document.querySelector(DOMstrings.inputValue).value
+            }
+        },
+
+        getDOMstrings: function(){
+            return DOMstrings;
+        }
+    }
 
 })();
 
 // GLOBAL APP CONTROLLER
 var controller = (function(budgetCtrl, UICtrl){
 
+    var DOM = UICtrl.getDOMstrings();
+
     var ctrlAddItem = function(){
 
         // 1. Get the field input data
-
+        var input = UICtrl.getinput();
+        console.log(input);
         // 2. Add the item to the bugdet controller
 
         // 3. Add the item to the UI
@@ -29,7 +53,7 @@ var controller = (function(budgetCtrl, UICtrl){
     }
     
     // Event Listeners
-    document.querySelector('.add__btn').addEventListener('click', ctrlAddItem);
+    document.querySelector(DOM.inputButton).addEventListener('click', ctrlAddItem);
 
     // Event listener for type
     document.addEventListener('keypress', function(e){
