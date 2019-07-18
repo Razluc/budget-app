2cb67dba1caad8c4edbf5869e16d3a9b043c514a
Razvan Popa <razvan.popa@softvision.ro>
2019-07-18 16:32:14 +0300

    5 - Expense and Income function constructor.

--- app.js
+++ app.js
@@ -2,6 +2,28 @@
 // BUDGET CONTROLLER
 var budgetController = (function(){
 
+    var Expense = function(id, description, value){ // fumction constructor
+        this.id             = id;
+        this.description    = description;
+        this.value          = value;
+    };
+
+    var Income = function(id, description, value){ // fumction constructor
+        this.id             = id;
+        this.description    = description;
+        this.value          = value;
+    };
+
+    var data = { // Were data will be stored.
+        allItems: {
+            exp: [],
+            inc: []
+        },
+        totals: {
+            exp: 0,
+            inc: 0
+        }
+    }    
 })();
 
 // UI CONTROLLER
